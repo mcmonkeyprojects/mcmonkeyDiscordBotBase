c3b0c1ee768aae0892037c550ade2c55ef4b83ab
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2021-10-15 21:35:47 -0700

    smarter cache prefill

--- DiscordMessageCache.cs
+++ DiscordMessageCache.cs
@@ -9,56 +9,39 @@ using System.Threading.Tasks;
 
 namespace DiscordBotBase
 {
+    /// <summary>Helper for caching Discord messages.</summary>
     public class DiscordMessageCache
     {
-        /// <summary>
-        /// How many messages to store per text channel.
-        /// </summary>
+        /// <summary>How many messages to store per text channel.</summary>
         public int MessagesPerChannel;
 
-        /// <summary>
-        /// The backing Discord bot.
-        /// </summary>
+        /// <summary>The backing Discord bot.</summary>
         public DiscordBot Bot;
 
-        /// <summary>
-        /// Construct the cache instance.
-        /// </summary>
+        /// <summary>Construct the cache instance.</summary>
         public DiscordMessageCache(DiscordBot _bot, int size)
         {
             Bot = _bot;
             MessagesPerChannel = size;
         }
 
-        /// <summary>
-        /// Represents a single cached message.
-        /// </summary>
+        /// <summary>Represents a single cached message.</summary>
         public struct CachedMessage
         {
-            /// <summary>
-            /// The ID of the sender of the message.
-            /// </summary>
+            /// <summary>The ID of the sender of the message.</summary>
             public ulong SenderID;
 
-            /// <summary>
-            /// The text content of the sent message.
-            /// </summary>
+            /// <summary>The text content of the sent message.</summary>
             public string Text;
 
-            /// <summary>
-            /// Attachment URLs, newline separated, if any.
-            /// </summary>
+            /// <summary>Attachment URLs, newline separated, if any.</summary>
             public string Attachments;
         }
 
-        /// <summary>
-        /// The caching data for a single channel.
-        /// </summary>
+        /// <summary>The caching data for a single channel.</summary>
         public class SingleChannelCache
         {
-            /// <summary>
-            /// Constructs the channel cache instance with a specified cache size.
-            /// </summary>
+            /// <summary>Constructs the channel cache instance with a specified cache size.</summary>
             public SingleChannelCache(int _size)
             {
                 Size = _size;
@@ -67,24 +50,16 @@ namespace DiscordBotBase
 
             }
 
-            /// <summary>
-            /// All cached message IDs, in order sent, for cache clearing.
-            /// </summary>
+            /// <summary>All cached message IDs, in order sent, for cache clearing.</summary>
             public Queue<ulong> IDsInSentOrder;
 
-            /// <summary>
-            /// A map of message IDs to cached data.
-            /// </summary>
+            /// <summary>A map of message IDs to cached data.</summary>
             public Dictionary<ulong, CachedMessage> Cache;
 
-            /// <summary>
-            /// How big the cache should be.
-            /// </summary>
+            /// <summary>How big the cache should be.</summary>
             public int Size;
 
-            /// <summary>
-            /// Adds a new message into this channel cache.
-            /// </summary>
+            /// <summary>Adds a new message into this channel cache.</summary>
             public void AddToCache(IMessage message)
             {
                 if (Size <= 0)
@@ -121,36 +96,41 @@ namespace DiscordBotBase
             return GetCacheForChannel(channelId).Cache.TryGetValue(messageId, out message);
         }
 
-        /// <summary>
-        /// A mapping of channel IDs to channel message caches.
-        /// </summary>
+        /// <summary>A mapping of channel IDs to channel message caches.</summary>
         public ConcurrentDictionary<ulong, SingleChannelCache> ChannelCaches = new ConcurrentDictionary<ulong, SingleChannelCache>();
 
-        /// <summary>
-        /// Gets the cache handler for a specific channel.
-        /// </summary>
+        /// <summary>Gets the cache handler for a specific channel.</summary>
         public SingleChannelCache GetCacheForChannel(ulong channelID)
         {
             return ChannelCaches.GetOrAdd(channelID, (id) => new SingleChannelCache(MessagesPerChannel));
         }
 
-        /// <summary>
-        /// Caches a message.
-        /// </summary>
+        /// <summary>Caches a message.</summary>
         public void CacheMessage(IMessage message)
         {
             GetCacheForChannel(message.Channel.Id).AddToCache(message);
         }
 
-        /// <summary>
-        /// Prefill the cache with previously sent messages.
-        /// </summary>
+        /// <summary>Prefill the cache with previously sent messages.</summary>
         public void Prefill()
         {
             if (MessagesPerChannel <= 0)
             {
                 return;
             }
+            if (MessagesPerChannel < 150)
+            {
+                Console.WriteLine("Performing initial cache pre-fill pass");
+                PrefillInternal(50);
+            }
+            Console.WriteLine("Performing full cache pre-fill");
+            PrefillInternal(MessagesPerChannel);
+            Console.WriteLine("Cache pre-fill complete.");
+        }
+
+        /// <summary>Internal prefill call, prefer to use <see cref="Prefill"/>.</summary>
+        public void PrefillInternal(int amountToFill)
+        {
             foreach (SocketGuild guild in Bot.Client.Guilds)
             {
                 foreach (SocketTextChannel channel in guild.TextChannels)
@@ -163,7 +143,7 @@ namespace DiscordBotBase
                     {
                         SingleChannelCache cache = GetCacheForChannel(channel.Id);
                         List<IMessage> messages = new List<IMessage>();
-                        channel.GetMessagesAsync(MessagesPerChannel).ForEachAwaitAsync(async col =>
+                        channel.GetMessagesAsync(amountToFill).ForEachAwaitAsync(async col =>
                         {
                             messages.AddRange(col);
                             await Task.Delay(100);
