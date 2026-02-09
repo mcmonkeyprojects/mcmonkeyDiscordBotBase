02d3862a057b0b9e41fad4279fe54b28e9b28160
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2026-02-08 22:11:12 -0800

    way easier restart call

--- CommandHandlers/CoreCommands.cs
+++ CommandHandlers/CoreCommands.cs
@@ -22,24 +22,12 @@ namespace DiscordBotBase.CommandHandlers
                 SendErrorMessageReply(command.Message, "Authorization Failure", "Nope! That's not for you!");
                 return;
             }
-            if (!File.Exists("./start.sh"))
-            {
-                SendErrorMessageReply(command.Message, "Cannot Comply", "Nope! That's not valid for my current configuration! (`start.sh` missing).");
-            }
             SendGenericPositiveMessageReply(command.Message, "Restarting", "Yes, boss. Restarting now...");
             Task.Factory.StartNew(() =>
             {
                 Task.Delay(1000).Wait();
-                Console.WriteLine("Initial restart trigger...");
-                Process.Start("bash", "./start.sh " + command.Message.Channel.Id);
-                Console.WriteLine("Shutdown start...");
-                for (int i = 0; i < 15; i++)
-                {
-                    Console.WriteLine("T Minus " + (15 - i));
-                    Task.Delay(1000).Wait();
-                }
                 Console.WriteLine("Shutdown!");
-                Environment.Exit(0);
+                Environment.Exit(42);
             });
             Bot.BotMonitor.StopAllLogic = true;
             Bot.Shutdown();
